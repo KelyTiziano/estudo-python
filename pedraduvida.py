3eb26974910811cdf6202b6f57b38b898f68b2d4
kely <kelytiziano@gmail.com>
2022-08-30 14:35:51 +0000

    rodou apenas a interação com os jogadores, o restante do jogo com os resultados não rodou

---
+++ pedraduvida.py
@@ -0,0 +1,19 @@
+jog_1=input(" jogador 1 escolha uma opcao,pedra,papel ou tesoura")
+jog_2=input(" jogador 2 escolha uma opcao,pedra,papel ou tesoura")
+if jog_1=="pedra" and jog_2=="tesoura":
+    print(f"vence {jog_1}")
+elif jog_1=="tesoura" and jog_2=="pedra":
+    print(f"vence {jog_2} ")
+if jog_1=="pedra" and jog_2=="papel":
+    print(f"vence {jog_2}")
+elif jog_1=="papel" and jog_2=="pedra":
+    print(f"vence {jog_1}")
+if jog_1=="papel" and jog_2=="tesoura":
+    print(f"vence {jog_2}")
+elif jog_1=="tesoura" and jog_2=="papel":
+    print(f"vence {jog_1}")
+
+#gostaria de criar o jogo e poder jogar para testar com meu filho e também ele poder ter interesse em programação
+
+
+
