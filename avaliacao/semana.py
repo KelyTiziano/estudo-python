b645551b30262fee46bb1968208481cfa7e38167
kely <kelytiziano@gmail.com>
2022-10-12 12:07:49 +0000

    lista 2 exercicio 13

---
+++ avaliacao/semana.py
@@ -0,0 +1,18 @@
+dia=int(input("digite o numero do dia da semana desejado sendo 1-domingo, 2-segunda, 3-terça, 4-quarta, 5-quinta, 6-sexta e 7-sabado "))
+
+if dia==1:
+    print("domingo")
+elif dia==2:
+    print("segunda")
+elif dia==3:
+    print("terça")
+elif dia==4:
+    print("quarta")
+elif dia==5:
+    print("quinta")
+elif dia==6:
+    print("sexta")
+elif dia ==7:
+    print("sabado")
+else:
+    print("valor invalido")
